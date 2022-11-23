96040e04ab1c49e9ba5c93289e1d6d397c145478
Juan Quiroz <juan.quiroz1009@gmail.com>
2022-11-23 13:04:29 -0500

    second-commit

--- src/SecondComponent.js
+++ src/SecondComponent.js
@@ -2,14 +2,22 @@ import React from "react";
 
 const SecondComponent = () => {
   //const libros = ["Hary Potter", "Games of Thrones", "Clean Code"];
-  const libros = [];
+  const libros = ["harry poter"];
   return (
     <div className="segundo-componente">
-      <ul>
-        {libros.map((element, index) => {
-          return <li key={index}>{element}</li>;
-        })}
-      </ul>
+        <h1>Listado de Libros</h1>
+        {libros.length >=1 ? (
+              <ul>
+              {libros.map((element, index) => {
+                return <li key={index}>{element}</li>;
+              })}
+            </ul>
+            )
+            :(
+            <p>No hay libros</p>
+        )
+    }
+
     </div>
   );
 };
