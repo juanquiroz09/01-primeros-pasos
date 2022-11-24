59b227f7e4b7495b6822d54b035f6efadad96de7
Juan Quiroz <juan.quiroz1009@gmail.com>
2022-11-24 09:48:01 -0500

    mouse-commit

--- src/EventosComponente.js
+++ src/EventosComponente.js
@@ -4,11 +4,34 @@ const EventosComponente = () => {
   const handleOnClick = (e, name) => {
     console.log("click " + name);
   };
+
+  const handleDoubleClick = (event, name_two) =>{
+    console.log("Double click " + name_two);
+  };
+
+  const mouse = (e, action) =>{
+    console.log("has " + action);
+  };
+
   return (
     <div>
       <h1>Eventos en React</h1>
-      <button onClick={e => {handleOnClick(e,"Juan")}}>Click!!</button>
-    </div>
+      <p>
+        {/*Evento click*/}
+       <button onClick={e => {handleOnClick(e,"Juan")}}>Click!!</button>
+      </p>
+      <p>
+        {/*Evento doble click */}
+        <button onDoubleClick={event => {handleDoubleClick(event, "Elias")}}>Double click</button>
+      </p>
+      <div id="caja" 
+      onMouseEnter={e => {mouse(e,"entrado")}}
+      onMouseLeave={e => {mouse(e,"salido")}}
+      >
+        {/*Evento onMouseEnter on MouseLeave */}
+        Pasa por encima
+      </div>
+          </div>
   );
 };
 
